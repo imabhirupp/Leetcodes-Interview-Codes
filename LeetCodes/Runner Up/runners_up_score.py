be7994ebd040b0e91ba8dc589c3b57115428bbde
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-10-22 14:26:06 +0530

    Update runners_up_score.py

--- LeetCodes/Runner Up/runners_up_score.py
+++ LeetCodes/Runner Up/runners_up_score.py
@@ -2,8 +2,10 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
 
-a = sorted(list(set(arr)))
+# Map doesn't return a list. Instead, it returns an iterator object and since sort is an attribute of list object, we will get error so we used sorted function which accepts an iterable and return a sorted list and then reassign the result to the original name.
+# Set function is a powerful tool to obtain unique values from a list. It automatically removes duplicates and returns a set object containing only the unique elements. We can then convert this set back into a list if needed.
 
+a = sorted(list(set(arr)))      
 if 2<= n <=10:
     print(a[-2])
         
